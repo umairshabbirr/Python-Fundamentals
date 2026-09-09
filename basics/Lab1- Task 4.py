7bfed850f910a6c2bc1489d149ea0325791d3d99
Umair Shabbir | Aspiring AI Engineer <umairshabbir415@gmail.com>
2026-09-09 15:54:55 +0500

    Student Marks Calculator
    A simple Python program that stores a student's name, roll number, and marks in three subjects. It calculates the total marks and average marks, then displays all the student details neatly.

---
+++ basics/Lab1- Task 4.py
@@ -0,0 +1,16 @@
+Student_name= "Ahmad"
+roll_no= "157"
+s1=35
+s2=45
+s3=27
+total= s1+s2+s3
+average= total/3
+
+print("----- Student Details -----")
+print("Name:", Student_name)
+print("Roll Number:", roll_no)
+print("Math Marks:", s1)
+print("English Marks:", s2)
+print("Computer Marks:", s3)
+print("Total Marks:", total)
+print("Average Marks:", average)
